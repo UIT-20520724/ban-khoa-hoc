e4b4c96ee6fed0f29d8c6378ca31a2cd5931f5c1
UIT-20520724 <20520724@gm.uit.edu.vn>
2023-12-11 21:06:22 +0700

    update Search-Page

--- frontend/src/components/SearchPage/SearchPage.js
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -5,11 +5,11 @@ import { Link, useNavigate, useParams, useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import CoursesDataService from "../../services/CoursesDataService";
 import Product from "../Product/Product";
-
+import banner from './banner_drop____mito_mt3_noctua_keycaps_name_1.jpg';
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 const SearchPage = () => {
-    const img = ['https://bizweb.dktcdn.net/100/453/393/themes/894913/assets/breadcrumb_image.png?1676281841878']
+    const img = [banner]
     const [courseResult, setCourseResult] = useState([]);
     const [countResult, setCountResult] = useState(0);
     const { _searchName } = useParams();
